16dff8bffb6f2109f983b5a8b88733edf78bba84
engineeredcurlz <32712461+engineeredcurlz@users.noreply.github.com>
2025-10-02 23:39:44 -0400

    comments

--- Python/two-sum.py
+++ Python/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return []
+        num_map = {} #initialize dictionary to store numbers and indicrd
+        for i, num in enumerate(nums): #iterate through the array with index abs value
+            complement = target - num #calculate the com[lement needed to reach target]
+            if complement in num_map: #check if complement exists in dictionary
+                return [num_map[complement], i] #if found, return the index of the complement and current index
+            num_map[num] = i #if complement is not found, add the current number and index to dictionary
+        return [] #if no solution\, return error
